5ef35eb678cd3f942e670d001b7eb9b37e4b63ad
Foad Kesheh <foad.kesheh@aurea.com>
2024-09-21 02:27:33 -0300

    fix: make sure to fetch the body, before calling the function

--- local.mjs
+++ local.mjs
@@ -5,37 +5,45 @@ import { handler } from './index.mjs';
 const port = 3500;
 
 const requestListener = async function (req, res) {
-  const lambdaEvent = {
-    version: '2.0',
-    routeKey: '$default',
-    rawPath: req.url,
-    rawQueryString: '',
-    headers: req.headers,
-    requestContext: {
-      domainName: req.headers.host || '',
-      http: {
-        method: req.method,
-        path: req.url,
-        protocol: req.httpVersion,
-        sourceIp: req.connection.remoteAddress,
-        userAgent: req.headers['user-agent'] || 'PostmanRuntime/7.41.1'
-      },
-      time: new Date().toISOString(),
-      timeEpoch: Date.now()
-    },
-    body: req.body,
-    isBase64Encoded: false
-  };
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
 
- try {
-    const response = await handler(lambdaEvent);
-    res.writeHead(response.statusCode, response.headers);
-    res.end(response.body);
- } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
- }
-}
+  req.on('end', async () => {
+    const lambdaEvent = {
+      version: '2.0',
+      routeKey: '$default',
+      rawPath: req.url,
+      rawQueryString: '',
+      headers: req.headers,
+      requestContext: {
+        domainName: req.headers.host || '',
+        http: {
+          method: req.method,
+          path: req.url,
+          protocol: req.httpVersion,
+          sourceIp: req.connection.remoteAddress,
+          userAgent: req.headers['user-agent'] || 'PostmanRuntime/7.41.1'
+        },
+        time: new Date().toISOString(),
+        timeEpoch: Date.now()
+      },
+      body: body,
+      isBase64Encoded: false
+    };
+
+    try {
+      const response = await handler(lambdaEvent);
+      res.writeHead(response.statusCode, response.headers);
+      res.end(response.body);
+    } catch (error) {
+      console.error(error);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  });
+};
 
 const server = http.createServer(requestListener);
 server.listen(port);
